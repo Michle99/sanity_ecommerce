879f38d5d8aded7dc8a38a3058ae50f7b9fe2089
Michle99 <sammiyonyx@gmail.com>
2023-03-10 13:52:41 -0500

    updated store name

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link className='logo-text' href='/'>SM Headphones</Link>
+        <Link className='logo-text' href='/'>Headieshop</Link>
       </p>
       <button type='button' className='cart-icon'
         onClick={() => setShowCart(true)}
